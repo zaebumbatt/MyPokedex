faf7b7f8fb15ac4a0e67fa57cb0841d444039afa
Nikita Ustiuzhanin <zaebumba@gmail.com>
2022-05-29 15:54:49 +0700

    Change models

--- models.py
+++ models.py
@@ -1,15 +1,35 @@
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.dialects.postgresql import ARRAY
+from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
 
 
-class MyPokedex(Base):
-    __tablename__ = 'my_pokedex'
+class User(Base):
+    __tablename__ = 'user'
 
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer)
-    pokemon_id = Column(String(30))
+    username = Column(String(30))
 
-    def __repr__(self):
-        return f'User {self.user_id} owns pokemon {self.pokemon_id}'
+
+class Pokemon(Base):
+    __tablename__ = 'pokemon'
+
+    id = Column(String(30), primary_key=True)
+    name = Column(String(255))
+    hp = Column(Integer)
+    image = Column(String(255))
+    rarity = Column(String(255))
+    supertype = Column(String(255))
+    subtypes = Column(ARRAY(String(255)))
+    types = Column(ARRAY(String(255)))
+
+
+class Pokedex(Base):
+    __tablename__ = 'pokedex'
+
+    user_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+    pokemon_id = Column(String(30), ForeignKey('pokemon.id'), primary_key=True)
+
+    user = relationship('User')
+    pokemon = relationship('Pokemon')
